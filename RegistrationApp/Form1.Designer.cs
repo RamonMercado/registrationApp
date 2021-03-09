f4ddd37be8d22cb8530029b80462d98e89cb5ef2
Ramon E. Mercado <mercado.ramon@outlook.com>
2021-03-09 13:44:05 -0400

    Example message

---
+++ RegistrationApp/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace RegistrationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.acceptTermscheckBox = new System.Windows.Forms.CheckBox();
+            this.emailTxtBox = new System.Windows.Forms.TextBox();
+            this.passwordTxtBox = new System.Windows.Forms.TextBox();
+            this.confirmPasswordTxtBox = new System.Windows.Forms.TextBox();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(82, 211);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "E-mail:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(82, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(82, 349);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Confirm Password:";
+            // 
+            // acceptTermscheckBox
+            // 
+            this.acceptTermscheckBox.AutoSize = true;
+            this.acceptTermscheckBox.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.acceptTermscheckBox.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.acceptTermscheckBox.Location = new System.Drawing.Point(122, 422);
+            this.acceptTermscheckBox.Name = "acceptTermscheckBox";
+            this.acceptTermscheckBox.Size = new System.Drawing.Size(220, 23);
+            this.acceptTermscheckBox.TabIndex = 4;
+            this.acceptTermscheckBox.Text = "Accept terms and conditions";
+            this.acceptTermscheckBox.UseVisualStyleBackColor = true;
+            this.acceptTermscheckBox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // emailTxtBox
+            // 
+            this.emailTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTxtBox.Location = new System.Drawing.Point(104, 233);
+            this.emailTxtBox.Name = "emailTxtBox";
+            this.emailTxtBox.Size = new System.Drawing.Size(257, 30);
+            this.emailTxtBox.TabIndex = 5;
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTxtBox.Location = new System.Drawing.Point(104, 302);
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.PasswordChar = '*';
+            this.passwordTxtBox.Size = new System.Drawing.Size(257, 30);
+            this.passwordTxtBox.TabIndex = 6;
+            this.passwordTxtBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // confirmPasswordTxtBox
+            // 
+            this.confirmPasswordTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confirmPasswordTxtBox.Location = new System.Drawing.Point(104, 371);
+            this.confirmPasswordTxtBox.Name = "confirmPasswordTxtBox";
+            this.confirmPasswordTxtBox.PasswordChar = '*';
+            this.confirmPasswordTxtBox.Size = new System.Drawing.Size(257, 30);
+            this.confirmPasswordTxtBox.TabIndex = 7;
+            this.confirmPasswordTxtBox.Leave += new System.EventHandler(this.confirmPassword);
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(159)))), ((int)(((byte)(176)))));
+            this.registerBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.registerBtn.FlatAppearance.BorderSize = 0;
+            this.registerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerBtn.ForeColor = System.Drawing.Color.White;
+            this.registerBtn.Location = new System.Drawing.Point(176, 484);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(103, 40);
+            this.registerBtn.TabIndex = 8;
+            this.registerBtn.Text = "Register";
+            this.registerBtn.UseVisualStyleBackColor = false;
+            this.registerBtn.Click += new System.EventHandler(this.registerBtn_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::RegistrationApp.Properties.Resources.close__1_;
+            this.pictureBox2.Location = new System.Drawing.Point(406, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(54, 39);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Close);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RegistrationApp.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(176, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.registerBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(472, 586);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.confirmPasswordTxtBox);
+            this.Controls.Add(this.passwordTxtBox);
+            this.Controls.Add(this.emailTxtBox);
+            this.Controls.Add(this.acceptTermscheckBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox acceptTermscheckBox;
+        private System.Windows.Forms.TextBox emailTxtBox;
+        private System.Windows.Forms.TextBox passwordTxtBox;
+        private System.Windows.Forms.TextBox confirmPasswordTxtBox;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ RegistrationApp/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Npgsql;
+
+namespace RegistrationApp
+{
+    public partial class Form1 : Form
+    {
+    
+        public Form1()
+        {
+            InitializeComponent();
+            diableBtn();
+        }
+
+        private void diableBtn()
+        {
+            if (passwordTxtBox.Text.Length == 0 && emailTxtBox.Text.Length == 0 && confirmPasswordTxtBox.Text.Length == 0)
+            {
+                registerBtn.Enabled = false;
+            }
+
+        } 
+        private void registerBtn_Click(object sender, EventArgs e)
+        {
+
+            //  SqlConnection con = new SqlConnection(@"Dsn=PostGreVS;uid=postgres");
+
+            emailTxtBox.Focus();
+            string email = emailTxtBox.Text;
+            string password = passwordTxtBox.Text;
+            string confirmPassword = confirmPasswordTxtBox.Text;
+            string acceptTerms;
+            if (acceptTermscheckBox.Checked == true)
+            {
+                acceptTerms = "Y";
+            }
+            else
+            {
+                acceptTerms = "N";
+            }
+            Console.WriteLine("Email: " + email);
+            Console.WriteLine("Password: " + password);
+            Console.WriteLine("Confirm Password: " + confirmPassword);
+            Console.WriteLine("Accepted Terms: " + acceptTerms);
+            //TestConnection();
+            insertData(email, password, acceptTerms);
+            this.Close();
+
+        }
+
+        private static void insertData(string email, string password, string acceptTerms)
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                string query = @"insert into userregistration values ('" + email + "','" + password + "', '" + acceptTerms + "');";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+                if (n == 1)
+                {
+                    Console.WriteLine("Record Inserted");
+                }
+            }
+        }
+        private static void TestConnection()
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                
+                if (con.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Connected");
+               
+                    ;
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }
+        }
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5432; User Id=postgres; Password = 123456; Database=Users;");
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Close(object sender, MouseEventArgs e)
+        {
+            this.Close();  //”this” refers to the form
+        }
+
+        private void confirmPassword(object sender, EventArgs e)
+        {
+            if(passwordTxtBox.Text == confirmPasswordTxtBox.Text && passwordTxtBox.Text.Length != 0)
+            {
+                registerBtn.Enabled=true;
+            }
+            else
+            {
+                registerBtn.Enabled = false;
+            }
+        }
+    }
+}
